fe978e32747ca9dea7ce8bd93c86d549960dfcc5
Janic Heen <janic@online.no>
2019-08-20 08:36:22 +0200

    Database repopulated

--- django-restapi/backend/pacovbase/models.py
+++ django-restapi/backend/pacovbase/models.py
@@ -10,7 +10,7 @@ class PACOV(models.Model):
     #Unique identification
     uuid = models.UUIDField(default=uuid_field.uuid4, unique=True, editable=False)
     # Type specification of PEPPAR
-    type = models.ForeignKey(PACOVType, on_delete=models.CASCADE, related_name='pacov_type')
+    type = models.ForeignKey(PACOVType, on_delete=models.CASCADE, related_name='pacov_type', blank=False, null=False)
     # The Name Element
     name = models.CharField(max_length=500, blank=True)
     # The two Time Elements
@@ -42,16 +42,15 @@ class PACOV(models.Model):
 
 # PACOV RELATIONAL MODEL
 class Relation(models.Model):
-
     #Unique identification
     uuid = models.UUIDField(default=uuid_field.uuid4, unique=True, editable=False)
     # Type specification of Relation
-    type = models.ForeignKey(RelationType, on_delete=models.CASCADE, related_name='relation_type')
+    type = models.ForeignKey(RelationType, on_delete=models.CASCADE, related_name='relation_type', blank=False, null=False)
     # Name
     name = models.CharField(max_length=500, blank=True )
     #PACOVs connected
-    pacovA = models.ForeignKey(PACOV, related_name="pacovA", on_delete=models.CASCADE)
-    pacovB = models.ForeignKey(PACOV, related_name="pacovB", on_delete=models.CASCADE)
+    pacovA = models.ForeignKey(PACOV, related_name="pacovA", on_delete=models.CASCADE, blank=False, null=False)
+    pacovB = models.ForeignKey(PACOV, related_name="pacovB", on_delete=models.CASCADE, blank=False, null=False)
     # The two Time Elements
     dateA = models.DateTimeField(blank=True, null=True)
     dateB = models.DateTimeField(blank=True, null=True)
@@ -72,7 +71,7 @@ class Relation(models.Model):
     #Function to define default value of name
     def save(self, *args, **kwargs):
         if not self.name:
-            self.name =  self.pepparA.name + " - " + self.type.name + "     - " + self.pepparB.name
+            self.name =  self.pacovA.name + " - " + self.type.name + " - " + self.pacovB.name
         super().save(*args, **kwargs)
         
     def __str__(self):
